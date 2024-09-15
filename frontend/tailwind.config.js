336039739f7d1714b33cd853c3b7660d6d0583b1
Ne3mah <83452116+Ne3mah@users.noreply.github.com>
2024-09-15 14:50:39 +0300

    Update tailwind.config.js

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -2,7 +2,9 @@ module.exports = {
   presets: [require('frappe-ui/src/utils/tailwind.config')],
   content: [
     './index.html',
-    './src/**/*.{vue,js,ts,jsx,tsx}'
+    './src/**/*.{vue,js,ts,jsx,tsx}',
+    './node_modules/frappe-ui/src/components/**/*.{vue,js,ts,jsx,tsx}',
+    '../node_modules/frappe-ui/src/components/**/*.{vue,js,ts,jsx,tsx}'
   ],
   theme: {
     extend: {},
